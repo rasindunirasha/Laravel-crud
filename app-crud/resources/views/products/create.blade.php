be03c62c4d4ba61576896da28c8606ebe605470c
rasindunirashagithub <rasindunirasha7@gmail.com>
2025-03-01 00:58:56 +0530

    create table

--- app-crud/resources/views/products/create.blade.php
+++ app-crud/resources/views/products/create.blade.php
@@ -7,5 +7,39 @@
 </head>
 <body>
     <h1>Create a Document </h1>
+    <div>
+        @if($errors->any())
+        <ul>
+            @foreach($errors->all() as $error)
+               <li>{{ $error }}</li>
+            @endforeach
+        </ul>
+
+        @endif
+    </div>
+
+    <form method="POST"  action="{{ route('products.store') }}">
+        @csrf
+        @method('post')
+        <div>
+            <label for="name">Name</label>
+            <input type="text" name="name" placeholder="name"/>
+        </div>
+        <div>
+            <label for="qty">Qty</label>
+            <input type="text" name="qty" placeholder="Qty"/>
+        </div>
+        <div>
+            <label for="price">Price</label>
+            <input type="text" name="price" placeholder="Price"/>
+        </div>
+        <div>
+            <label for="description">Description</label>
+            <input type="text" name="description" placeholder="Description"/>
+        </div>
+        <div>
+          <input type="submit" value ="Save a New Product">
+        </div>
+    </form>
 </body>
 </html>
