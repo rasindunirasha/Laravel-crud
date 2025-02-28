1b25ece7948fb3fce17354544ea122ceb5aebe40
rasindunirashagithub <rasindunirasha7@gmail.com>
2025-03-01 00:57:26 +0530

    update product php

--- app-crud/app/Models/product.php
+++ app-crud/app/Models/product.php
@@ -4,8 +4,10 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 class product extends Model
 {
+    use HasFactory;
     protected $fillable = [
        'name',
        'qty',
