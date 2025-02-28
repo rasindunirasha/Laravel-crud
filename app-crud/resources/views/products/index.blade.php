5e4f2322bb394eae1cd34be90e0e11364163013a
rasindunirashagithub <rasindunirasha7@gmail.com>
2025-03-01 00:59:43 +0530

    create edit.balde.php file

--- app-crud/resources/views/products/index.blade.php
+++ app-crud/resources/views/products/index.blade.php
@@ -7,6 +7,51 @@
 </head>
 <body>
     <h1>Product</h1>
-    <div>index</div>
+    <div>
+        @if(session()->has('success'))
+        <div>
+            {{ session('success') }}
+        </div>
+        @endif
+    </div>
+    <div>
+        <table border="1">
+            <tr>
+                <th>ID</th>
+                <th>Name</th>
+                <th>Qty</th>
+                <th>Price</th>
+                <th>Description</th>
+                <th>Edit</th>
+                <th>Delete</th>
+            </tr>
+            @foreach ($products as $product )
+            <tr>
+                <td>{{ $product->id }}</td>
+                <td>{{ $product->name}}</td>
+                <td>{{ $product->qty }}</td>
+                <td>{{ $product->price}}</td>
+                <td>{{ $product->description}}</td>
+                <td>
+                <a href="{{ route('products.edit', $product->id) }}">Edit</a>
+                </td>
+
+                <td>
+                <form method="POST" action="{{ route('products.destroy', $product->id) }}" onsubmit="return confirm('Are you sure?');">
+                 @csrf
+                 @method('DELETE')
+                <button type="submit">Delete</button>
+                </form>
+
+
+
+
+                </td>
+
+            </tr>
+            
+            @endforeach
+        </table>
+    </div>
 </body>
 </html>
--- app-crud/routes/web.php
+++ app-crud/routes/web.php
@@ -11,3 +11,10 @@ Route::get('/product',[ProductController::class,'index'])->name('products.index'
 
 Route::get('/product/create',[ProductController::class,'create'])->name('products.create');
      
+Route::post('/product',[ProductController::class,'store'])->name('products.store');
+
+Route::get('/product/{id}/edit', [ProductController::class, 'edit'])->name('products.edit'); // Show edit form
+Route::patch('/product/{id}', [ProductController::class, 'update'])->name('products.update'); // Handle update request
+
+Route::delete('/product/{id}', [ProductController::class, 'destroy'])->name('products.destroy');
+
